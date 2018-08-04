4bbb4cb60525ff5d75507fcf675a9979e97b9205
Sumeet Santani <ssantani@syr.edu>
2018-08-04 09:36:26 -0400

    Update SLR for Python.py

--- SLR for Python.py
+++ SLR for Python.py
@@ -1,39 +1,44 @@
-# Simple Linear Regression
+# Simple Linear Regression. Do companies really consider years of experience to determine a potential employee's salary?
 
+
+#Python Code
+
+#Step 1
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
 
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
+#Step 2
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+
+
+#Step 3
 
 # Fitting Simple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
+#Step 4
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
 
+
+#Step 5
 # Visualising the Training set results
 plt.scatter(X_train, y_train, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Training set)')
+plt.title('Duration of Work Experience v/s Salar (in USD) (Training set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
@@ -41,7 +46,7 @@ plt.show()
 # Visualising the Test set results
 plt.scatter(X_test, y_test, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Test set)')
+plt.title('Duration of Work Experience v/s Salar (in USD)(Test set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
